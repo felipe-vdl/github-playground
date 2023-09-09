16b95ab31f7ddcda428975bf49475f3db3e6c5be
felipe-pmm <felipe.vidal.mesquita@gmail.com>
2023-09-09 14:48:27 -0300

    Keyframe Animations

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,18 @@ export default function App() {
   ]);
   const [cartProducts, setCartProducts] = useState<CartProduct[]>([]);
   const [cartIsOpen, setCartIsOpen] = useState<boolean>(false);
+  const [cartIsClosing, setCartIsClosing] = useState<boolean>(true);
 
   const handleClick = () => {
-    setCartIsOpen(st => !st);
+    if (cartIsOpen) {
+      setCartIsClosing(true);
+      setTimeout(() => {
+        setCartIsOpen(false);
+      }, 200);
+    } else {
+      setCartIsOpen(true);
+      setCartIsClosing(false);
+    }
   }
 
   const handleAdd = (product: Product, quantity: number) => {
@@ -48,20 +57,20 @@ export default function App() {
   }
 
   return (
-    <div className="bg-slate-800 h-screen bg-gradient-to-br from-rose-900/20 to-blue-900/10 flex flex-col">
+    <div className="bg-slate-800 min-h-screen bg-gradient-to-br from-rose-900/20 to-blue-900/10 flex flex-col">
       <header className="shadow-md flex justify-between px-20 text-white/90 bg-slate-400/10 py-3">
         <h1 className="bg-clip-text text-transparent bg-gradient-to-br from-red-500 to-indigo-500 text-3xl font-bold tracking-wide">
           Drugs 4 You
         </h1>
-        <button onClick={handleClick} className="flex items-center gap-3 bg-indigo-500 p-2 rounded-full hover:bg-indigo-600 transition-all duration-150">
+        <button onClick={handleClick} className="flex items-center gap-3 bg-indigo-500 p-2 rounded-full hover:bg-indigo-600 transition-all duration-200">
           <svg xmlns="http://www.w3.org/2000/svg" className="mx-1" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
           </svg> Your Cart <span className="bg-slate-800/60 p-1 rounded-full px-3">{cartProducts.length}</span>
         </button>
       </header>
-      <main className="flex-1 px-8 pt-8 flex flex-col gap-8 text-white/75 relative">
+      <main className="overflow-hidden flex-1 p-8 flex flex-col gap-8 text-white/75 relative">
         {cartIsOpen && <>
-          <div className="bg-slate-800 drop-shadow absolute top-0 right-0 h-full z-20 flex flex-col">
+          <div className={`bg-slate-800 drop-shadow absolute top-0 right-0 h-full z-20 flex flex-col ${cartIsClosing ? "animation-slide-out" : "animation-slide-in"}`}>
             <h1 className="text-lg font-bold border-b p-4 border-indigo-500">Your Cart</h1>
             {cartProducts.map(product =>
               <div className="flex flex-col border-b border-white/20 py-4" key={`${product.id}-${product.name}`}>
@@ -72,9 +81,9 @@ export default function App() {
                 </div>
               </div>
             )}
-            <h2 className="italic text-center text-indigo-400 font-bold mt-auto mb-2">Total: ${cartProducts.reduce((acc, product) => product.price * product.quantity + acc, 0)/100}</h2>
+            <h2 className="italic text-center text-indigo-400 font-bold my-3">Total: ${cartProducts.reduce((acc, product) => product.price * product.quantity + acc, 0) / 100}</h2>
           </div>
-          <div onClick={handleClick} className="top-0 left-0 z-10 backdrop-blur-[1px] w-full h-full bg-black/30 absolute" />
+          <div onClick={handleClick} className={`top-0 left-0 z-10 backdrop-blur-[1px] w-full h-full bg-black/30 absolute ${cartIsClosing ? "animation-fade-out" : "animation-fade-in"}`} />
         </>}
         <section className="bg-slate-700/60 p-4 px-8 rounded shadow text-center">
           <h2 className="text-2xl mb-4">All the Drugs 4 You</h2>
@@ -95,7 +104,7 @@ export default function App() {
                   <input type="hidden" value={product.id} name="id" />
                   <input name="quantity" id={`amount-${product.id}`} type="number" step={1} min={1} className="rounded w-12 p-1 text-slate-800" />
                 </div>
-                <button className="rounded-full bg-indigo-500 p-1 hover:bg-indigo-600 transition-all duration-150">+ Add</button>
+                <button className="rounded-full bg-indigo-500 p-1 hover:bg-indigo-600 transition-all duration-200">+ Add</button>
               </form>
             </div>
           )}
