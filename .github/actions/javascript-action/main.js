085701247903cc716532c8dceef3e0217d8b8cee
felipe-pmm <felipe.vidal.mesquita@gmail.com>
2023-09-21 23:48:09 -0300

    using @actions/exec

--- .github/actions/javascript-action/main.js
+++ .github/actions/javascript-action/main.js
@@ -1,18 +1,20 @@
 import core from "@actions/core"
 //const github = require('@actions/github');
-//const exec = require('@actions/exec');
+import exec from "@actions/exec";
 
 console.log('Hello from Node 20!');
 
 // Getting Inputs
-  // Using env
-  console.log(process.env.INPUT_MESSAGE);
+// Using env
+console.log(process.env.INPUT_MESSAGE);
 
-  // Using core
-  const message = core.getInput('message', {
-    required: true,
-  });
-  console.log(message);
+// Using core
+const message = core.getInput('message', {
+  required: true,
+});
+console.log(message);
 
 // Setting Outputs
 core.setOutput('greetings', 'Greetings from the Output!!');
+
+exec.exec('echo "Hello from the exec package!"');
