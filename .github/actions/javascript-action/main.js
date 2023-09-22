fe50a6950317ceda10c7697e1648f87f632e76cf
felipe-pmm <felipe.vidal.mesquita@gmail.com>
2023-09-21 23:52:39 -0300

    @actions/github

--- .github/actions/javascript-action/main.js
+++ .github/actions/javascript-action/main.js
@@ -1,5 +1,5 @@
 import core from "@actions/core"
-//const github = require('@actions/github');
+import github from "@actions/github";
 import exec from "@actions/exec";
 
 console.log('Hello from Node 20!');
@@ -17,4 +17,15 @@ console.log(message);
 // Setting Outputs
 core.setOutput('greetings', 'Greetings from the Output!!');
 
-exec.exec('echo "Hello from the exec package!"');
+// @actions/exec — Send commands to the CLI.
+const main = async () => {
+  const result = await exec.exec('echo "Hello from the exec package!"');
+  console.log(result);
+}
+main();
+
+// @actions/github — Some context objects (but not all of them).
+console.log(github.context);
+
+// We can also use the github.octoKit() to communicate with GitHub APIs.
+// github.getOctokit()
