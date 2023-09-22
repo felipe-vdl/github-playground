fc52c5890959a2d914748f4a8d24c833863240db
felipe-pmm <felipe.vidal.mesquita@gmail.com>
2023-09-21 23:13:55 -0300

    actions/exec,github

--- .github/actions/javascript-action/main.js
+++ .github/actions/javascript-action/main.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
+// const github = require('@actions/github');
+// const exec = require('@actions/exec');
 
 console.log('Hello from Node 20!');
-
 console.log(process.env.INPUT_MESSAGE);
-
 core.setOutput('greetings', 'Greetings from the Output!!');
